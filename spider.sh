5359ffd521650f123bf9fc19815dd198dd7f83a6
m2q1n9 <m2q1n9@users.noreply.github.com>
2015-03-20 02:41:39 +0800

    Add files

---
+++ spider.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cp -f index_html index.html
+rm -fr blog-cn && mkdir blog-cn
+
+REGEXP=".*\/\(yinwang.org\/\(blog-cn\/.*\)\/\(.*\)\/\(.*\)\/.*\)\\\">\(.*\)<.*"
+wget yinwang.org -O -|\
+gsed -n "/$REGEXP/p" | gsed "s/$REGEXP/\1|\2-\3-\4|\5/g" |\
+gawk -F '|' '{ h=""$2" "$3".html"; c="\
+wget "$1" -O \""h"\";\
+echo \"<li><a href=\\\""h"\\\">"$3"</a></li>\">>index.html\
+"; print c; system(c) }'
+echo "<br/></body></html>">>index.html
